65c84b42e60b06625f330c609c41ccaa994144d2
jasen8651 <jasen20834@gmail.com>
2022-10-20 14:22:14 +0900

    자바수정2222

--- src/java001/main011.java
+++ src/java001/main011.java
@@ -5,6 +5,13 @@ public class main011 {
 	public static void main(String[] args) {
 		
 		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		System.out.println();
+		System.out.println();
+		
 	}
 
 }
